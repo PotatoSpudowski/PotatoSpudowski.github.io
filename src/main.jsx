8498454c84066a23ad8071c5f4813f880ade0157
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2026-06-15 03:13:06 +0530

    add posthog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import posthog from 'posthog-js'
 import App from './App'
 import './styles.css'
+
+posthog.init('phc_vos7uxTUoxSbzjH6RBEt8x7aNibKub5jfNiLXsMeJR3c', {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2026-05-30',
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
